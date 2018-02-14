29410900aba0274c352991a4667942a478e68a95
William Lewis <william.lewis@wework.com>
2018-02-14 12:02:41 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,26 @@
+
+
+def turn_count(board)
+  iterator = 0
+  board.each do |space|
+    if space != ' '
+      iterator += 1
+    else
+      pass
+    end
+  return iterator
+end
+
+
+
+=begin
+def current_player(board)
+  count = turn_count(board)
+  
+  if count.odd?
+    'O' # returns this token
+  else
+    'X' # returns this token instead
+  end
+end
+=end
